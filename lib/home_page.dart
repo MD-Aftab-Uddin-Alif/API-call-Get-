095416f401e8d1e30af9226f549079b207d3e480
MD-Aftab-Uddin-Alif <alifbaiust@gmail.com>
2023-04-06 01:31:44 +0600

    fourth commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:getapi_call/Provider/user_provider.dart';
 import 'package:provider/provider.dart';
+import 'dart:ui';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -20,8 +22,16 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final userProvider=Provider.of<UserProvider>(context);
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Center(child: Text("Get API calling")),
+        backgroundColor: Colors.black45,
+        title: Center(child: Text("Get API calling",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25,
+          color: Colors.brown,
+        ),
+        )),
       ),
       body: SafeArea(
         child: userProvider.isLoading==true
@@ -29,10 +39,62 @@ class _HomePageState extends State<HomePage> {
         )
         :ListView.builder(itemBuilder:
             (context, itemIndex){
-          return ListTile(
-            leading: Text(userProvider.allUserList[itemIndex].id.toString()),
-            title: Text(userProvider.allUserList[itemIndex].name.toString()),
-            subtitle: Text(userProvider.allUserList[itemIndex].email.toString()),
+          return Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 30,
+                  child: Text(userProvider.allUserList[itemIndex].id.toString(),
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              )),
+              title: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(child: Text(userProvider.allUserList[itemIndex].name.toString(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ))),
+              subtitle: Column(
+                children: [
+                  SizedBox(height: 2,),
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text(userProvider.allUserList[itemIndex].email.toString(),
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ))),
+                  SizedBox(height: 2,),
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white12,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text(userProvider.allUserList[itemIndex].phone.toString(),
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ))),
+                ],
+              ),
+            ),
           );
         },
           itemCount: userProvider.allUserList.length,
